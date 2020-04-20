77417d51497206a15ae429bd4259768edf151de5
Sebastián Pérez Hernández <sebastian.perezh@usantoto.edu.co>
2020-04-20 10:53:26 -0500

    P2_T10_AVERAGE_HEIGHT
    P2_T10_AVERAGE_HEIGHT

---
+++ P2_T6_METHODS_WITH_TIME/src/com/usta/sistemas/Main.java
@@ -0,0 +1,11 @@
+package com.usta.sistemas;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //AUTHOR: Sebastián Pérez Hernández
+        //DATE: 20/04/2020
+        //DESCRICPTION
+    }
+}
