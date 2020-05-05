6db67059bb13d5b411e6b8042a363a69c12a341f
Sebastián Pérez Hernández <sebastian.perezh@usantoto.edu.co>
2020-05-05 10:44:44 -0500

    P2_T27_VECTOR EXERCISES
    P2_T27_VECTOR EXERCISES

---
+++ P2_T28_INPUT_N_NUMBERS/src/com/usta/sistemas/Main.java
@@ -0,0 +1,25 @@
+package com.usta.sistemas;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //AUTHOR:Sebastián Pérez Hernández
+        //DATE: 05/05/2020
+        //DESCRIPTION:This software
+        f_menu();
+    }
+    public static void f_menu() {
+        System.out.println(".......................");
+        System.out.println(".   SoftInputNumbers  .");
+        System.out.println(".   V1.0 05/05/2020   .");
+        System.out.println(". Created by: SebasP  .");
+        System.out.println(".......................");
+        System.out.println("Input the total numbers");
+        Scanner keyboard = new Scanner(System.in);
+        
+    }
+    public static void
+}
